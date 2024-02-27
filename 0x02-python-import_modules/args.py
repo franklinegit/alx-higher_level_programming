40adea4ae5b76914013ae5182456b29084fa1bf5
franklinegit <oddfrankline@gmail.com>
2024-02-27 21:23:45 +0000

    How to use argv to check number of arguments and print them

---
+++ 0x02-python-import_modules/args.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Program that prints the number of and list of arguments."""
+    import sys
+
+    def noargs():
+        """The function prints the number of arguments passed to the script"""
+        
+        print("Finding number of arguments")
+        if len(sys.argv) == 1:
+            print("No arguments.")
+        
+        elif len(sys.argv) == 2:
+            print("{} argument.".format(len(sys.argv) - 1))
+        
+        else:
+            print("{} arguments.".format(len(sys.argv) - 1))
+
+        print("")
+            
+    def printargs():
+        """The function prints the actual arguments passed to the script"""
+        
+        print("Finding number of arguments and printing them as a list")
+        no = len(sys.argv)
+        
+        if len(sys.argv) - 1 == 0:
+            print("No arguments.")
+            
+        elif len(sys.argv) - 1 == 1:
+            print("{} argument.".format(no - 1))
+            
+        else:
+            print("{} arguments.".format(no - 1))
+        
+        for i in range(1, no):
+            print("{} {}".format(i, sys.argv[i]))
+
+        print("")
+
+    noargs()
+    printargs()
