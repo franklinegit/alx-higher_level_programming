2f3fd6f07c37fd2844e32b350dcbb6d6e7121396
franklinegit <oddfrankline@gmail.com>
2024-02-29 09:59:59 +0000

    Added string to list

--- 0x03-python-data_structures/0-main.py
+++ 0x03-python-data_structures/0-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_list_integer = __import__('0-print_list_integer1').print_list_integer
 
-my_list = [1, 2, 3, 4, 5, 78, 45]
+my_list = [1, 2, 3, "world", 4, 5, 78, 45, "hello"]
 print_list_integer(my_list)
