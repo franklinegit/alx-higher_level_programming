e85ed7a00359977914bc0a998a2f1d8ce2aa037c
franklinegit <oddfrankline@gmail.com>
2024-03-01 08:06:53 +0000

    Alternative implementation of task 5

---
+++ 0x04-python-more_data_structures/5-number_keys1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """function that returns the number of keys in a dictionary.
+    
+    Args:
+        a-dictionary: A dictionary
+        
+    Return:
+        Number of keys in dictionary
+        
+    """
+    keys = a_dictionary.keys()
+    return (len(keys))
