7a6ee551d75cc3f449dac3001f3d3e55b63eef15
franklinegit <oddfrankline@gmail.com>
2024-05-07 13:12:08 +0000

    Added documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
+"""
+Defines a square
+
+Raises:
+    TypeError: If type of size is not int
+    ValueError: If value of size is less than zero
+"""
+
 
 class Square:
-    
+    """
+    a class for square
+    """
     def __init__(self, size=0):
-        
+        """_summary_
+
+        Args:
+            size (int): Size of square. Defaults to 0.
+
+        Raises:
+            TypeError: If type of size is not int
+            ValueError: If value of size is less than zero
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
