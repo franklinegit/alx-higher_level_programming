47de95a8c404fea3675307c07d1f131858c97601
franklinegit <oddfrankline@gmail.com>
2024-05-07 13:00:21 +0000

    Added documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+1-square
+
+Module with a class square
+"""
+
 
 class Square:
-    
+    """
+    class Square that defines a square by: (based on 0-square.py)
+    """
     def __init__(self, size):
+        """
+        Constructor
+
+        Args:
+            size (int): size of square
+        """
         self.__size = size
