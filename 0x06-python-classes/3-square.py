fc87b66d0f53c5d718bf19187b5f3848ec97f4c7
franklinegit <oddfrankline@gmail.com>
2024-05-07 13:19:26 +0000

    Added documentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
+"""_summary_
+
+Raises:
+    TypeError: If size is not of type int
+    ValueError: If size is not >= 0
+"""
+
 
 class Square:
     """class Square that defines a square
-    
+
     methods:
         def area(self):
             Finds area of the square
     """
-    
+
     def __init__(self, size=0):
         """constructor method
-        
+
         Args:
             size (int ): Size of square side
         """
-        
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-            
+
     def area(self):
         """Function that finds area of the square
-        
+
         Returns:
             int: Area
         """
-        return(self.__size ** 2)
+        return (self.__size ** 2)
